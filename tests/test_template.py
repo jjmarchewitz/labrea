afc84eba72815483587e5fca6a5ecce8a81dd002
Jake Marchewitz <jjmarchewitz@gmail.com>
2025-05-21 19:34:11 -0400

    test_template

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,70 +5,72 @@ from labrea.template import Template
 
 
 def test_basic():
-    template = Template('{GREETING}, {:name:}!', name='World')
-    options = {'GREETING': 'Hello'}
+    template = Template("{GREETING}, {:name:}!", name="World")
+    options = {"GREETING": "Hello"}
 
-    assert template(options) == 'Hello, World!'
+    assert template(options) == "Hello, World!"
     template.validate(options)
-    assert template.keys(options) == {'GREETING'}
-    assert template.explain(options) == {'GREETING'}
-    # JAKE:
+    assert template.keys(options) == {"GREETING"}
+    assert template.explain(options) == {"GREETING"}
+    assert template.evaluate_options(options) == {"GREETING": "Hello"}
 
 
 def test_malformed():
     with pytest.raises(ValueError):
-        Template('{GREETING} {:name:}')
+        Template("{GREETING} {:name:}")
 
     with pytest.warns(UserWarning):
-        Template('{GREETING}', name='World')
+        Template("{GREETING}", name="World")
 
 
 def test_missing():
-    template = Template('{GREETING}, {:name:}!', name='World')
+    template = Template("{GREETING}, {:name:}!", name="World")
     options = {}
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template(options)
-        assert excinfo.value.key == 'GREETING'
+        assert excinfo.value.key == "GREETING"
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template.validate(options)
-        assert excinfo.value.key == 'GREETING'
+        assert excinfo.value.key == "GREETING"
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template.keys(options)
-        assert excinfo.value.key == 'GREETING'
+        assert excinfo.value.key == "GREETING"
 
-    assert template.explain(options) == {'GREETING'}
+    assert template.explain(options) == {"GREETING"}
 
 
 def test_confectioner_templating():
-    template = Template('{A} {B}{:punc:}', punc='!')
-    present = {'A': '{C}', 'B': '{D}', 'C': 'Hello', 'D': 'World'}
-    missing = {'A': '{C}', 'B': '{D}', 'C': 'Hello'}
+    template = Template("{A} {B}{:punc:}", punc="!")
+    present = {"A": "{C}", "B": "{D}", "C": "Hello", "D": "World"}
+    missing = {"A": "{C}", "B": "{D}", "C": "Hello"}
 
-    assert template(present) == 'Hello World!'
+    assert template(present) == "Hello World!"
     template.validate(present)
-    assert template.keys(present) == {'A', 'B', 'C', 'D'}
+    assert template.keys(present) == {"A", "B", "C", "D"}
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template(missing)
-        assert excinfo.value.key == 'D'
+        assert excinfo.value.key == "D"
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template.validate(missing)
-        assert excinfo.value.key == 'D'
+        assert excinfo.value.key == "D"
 
     with pytest.raises(KeyNotFoundError) as excinfo:
         template.keys(missing)
-        assert excinfo.value.key == 'D'
+        assert excinfo.value.key == "D"
 
-    assert template.explain(present) == {'A', 'B', 'C', 'D'}
-    assert template.explain(missing) == {'A', 'B', 'C', 'D'}
-    assert template.explain() == {'A', 'B'}
+    assert template.explain(present) == {"A", "B", "C", "D"}
+    assert template.explain(missing) == {"A", "B", "C", "D"}
+    assert template.explain() == {"A", "B"}
 
 
 def test_repr():
-    assert repr(Template('{GREETING}, {:name:}!', name='World')
-                ) == "Template('{GREETING}, {:name:}!', name=Value('World'))"
-    assert repr(Template('{GREETING}')) == "Template('{GREETING}')"
+    assert (
+        repr(Template("{GREETING}, {:name:}!", name="World"))
+        == "Template('{GREETING}, {:name:}!', name=Value('World'))"
+    )
+    assert repr(Template("{GREETING}")) == "Template('{GREETING}')"
